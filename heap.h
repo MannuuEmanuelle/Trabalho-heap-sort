8881d1f668a3cf97ca3893a78771d13d41f7800b
Emanuelle <capic.2024118tads0027@aluno.ifpi.edu.br>
2025-08-15 16:08:43 -0300

    Declaração das funções

---
+++ heap.h
@@ -0,0 +1,11 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <stdio.h>
+
+
+void heapify(int array[], int n, int i);
+void heapSort(int array[], int n);
+void printArray(int array[], int n);
+
+#endif
